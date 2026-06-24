47680e68c1ffa14f8873ab72f34c2989b8d77238
Rishabh Singh <igrisabop@gmail.com>
2026-06-24 20:55:34 +0530

    fixing

--- frontend/Dashboard.py
+++ frontend/Dashboard.py
@@ -5,7 +5,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-BACKEND_URL = 'https://customer-support-backend-iwhm.onrender.com'
+BACKEND_URL = st.secrets["BACKEND_URL"]
 
 # --------------------------------------------------
 # Page Config
